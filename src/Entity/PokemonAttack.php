c8ff00788b11f273434a49c91f1bb989029a11be
Kokuga <damien.mizzi@live.fr>
2021-07-19 09:56:52 +0200

    Make PokemonAttack with composite primary key

--- src/Entity/PokemonAttack.php
+++ src/Entity/PokemonAttack.php
@@ -1,77 +1,59 @@
 <?php
 
-namespace App\Entity;
+    namespace App\Entity;
 
-use App\Repository\PokemonAttackRepository;
-use Doctrine\ORM\Mapping as ORM;
-
-/**
- * @ORM\Entity(repositoryClass=PokemonAttackRepository::class)
- */
-class PokemonAttack
-{
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+    use App\Repository\PokemonAttackRepository;
+    use Doctrine\ORM\Mapping as ORM;
 
     /**
-     * @ORM\ManyToOne(targetEntity=Pokemon::class, inversedBy="attacks")
-     * @ORM\JoinColumn(nullable=false)
+     * @ORM\Entity(repositoryClass=PokemonAttackRepository::class)
      */
-    private $pokemon;
-
-    /**
-     * @ORM\ManyToOne(targetEntity=Attack::class, inversedBy="pokemons")
-     * @ORM\JoinColumn(nullable=false)
-     */
-    private $attack;
-
-    /**
-     * @ORM\Column(type="integer")
-     */
-    private $level;
-
-    public function getId(): ?int
+    class PokemonAttack
     {
-        return $this->id;
+        /**
+         * @ORM\Id
+         * @ORM\ManyToOne(targetEntity=Pokemon::class, inversedBy="attacks")
+         * @ORM\JoinColumn(nullable=false)
+         */
+        private $pokemon;
+
+        /**
+         * @ORM\Id
+         * @ORM\ManyToOne(targetEntity=Attack::class, inversedBy="pokemons")
+         * @ORM\JoinColumn(nullable=false)
+         */
+        private $attack;
+
+        /**
+         * @ORM\Column(type="integer")
+         */
+        private $level;
+
+        public function __construct(Pokemon $pokemon, Attack $attack)
+        {
+            $this->pokemon = $pokemon;
+            $this->attack = $attack;
+        }
+
+        public function getPokemon(): ?Pokemon
+        {
+            return $this->pokemon;
+        }
+
+        public function getAttack(): ?Attack
+        {
+            return $this->attack;
+        }
+
+        public function getLevel(): ?int
+        {
+            return $this->level;
+        }
+
+        public function setLevel(int $level): self
+        {
+            $this->level = $level;
+
+            return $this;
+        }
     }
-
-    public function getPokemon(): ?Pokemon
-    {
-        return $this->pokemon;
-    }
-
-    public function setPokemon(?Pokemon $pokemon): self
-    {
-        $this->pokemon = $pokemon;
-
-        return $this;
-    }
-
-    public function getAttack(): ?Attack
-    {
-        return $this->attack;
-    }
-
-    public function setAttack(?Attack $attack): self
-    {
-        $this->attack = $attack;
-
-        return $this;
-    }
-
-    public function getLevel(): ?int
-    {
-        return $this->level;
-    }
-
-    public function setLevel(int $level): self
-    {
-        $this->level = $level;
-
-        return $this;
-    }
-}
